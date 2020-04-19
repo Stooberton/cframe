0848cb6f8d2ce0ebe0d33a216b09f5f47d77caeb
Stoob <steejerbs@gmail.com>
2020-04-18 20:27:40 -0500

    Overhauled families
    Added ENT:GetAllChildren
    Added Contraption.GetPhysicalEnts(Contraption)

--- lua/cframe/server/families.lua
+++ lua/cframe/server/families.lua
@@ -1,112 +1,182 @@
-local function GetAllChildren(Ent, Output)
-    if Output then
-        Output[Ent] = true
-    else
-        Output = {}
-    end
+-- Builds "families"
+-- Ancestor: root of a parent chain, the physical entity all children are parented to
+-- Family: All children (and their children) of an ancestor
 
-    for K in pairs(Ent:GetChildren()) do
-        GetAllChildren(K, Output)
-    end
+do -- Add to the contraption library ------------
+	function Contraption.GetParentedEnts(C)
+		local Out = {}
 
-    return Output
+		if C.Families then -- If there are any parented ents on this contraption
+			for Family in pairs(C.Families) do -- Go through every family
+				for E in pairs(Family) do -- and every ent on that family
+					if not E.CFW.Physical then -- and if it isnt physical
+						Out[E] = true -- Add to our list
+					end
+				end
+			end
+		end
+
+		return Out
+	end
+
+	function Contraption.GetPhysicalEnts(C)
+		local Out = {}
+
+		for Ent in pairs(C.Ents) do -- Go through every entity on a contraption
+			if Ent.CFW.Physical then -- and if it's physical
+				Out[Ent] = true -- Add to our list
+			end
+		end
+	end
 end
 
-hook.Add("OnContraptionCreate", "Families", function(C)
-    C.Families = {}
-end)
-
 hook.Add("OnContraptionConnect", "Families", function(A, B, Parent)
-    if not Parent then return end -- Only care about parent actions
+	if not Parent then return end -- Only care about parent actions
 
-    local Ancestor = B.CFW.Ancestor or B
-    local Family   = Ancestor.CFW.Family or {}
-    local OldFam   = A.CFW.Family
+	local Ancestor = B:GetAncestor()
+	local Family   = A:GetFamily()
+	local AncFam   = Ancestor:GetFamily()
 
-    Family[A] = true -- Add A to the family
+	if Family and AncFam then -- Merge
+		print("merge families")
+		if #Family > #AncFam then
+			for Ent in pairs(AncFam) do
+				AncFam[Ent] = nil
+				Family[Ent] = true
 
-    if OldFam then -- Transfer old family, if it exists, to new family
-        for Ent in pairs(OldFam) do
-            OldFam[Ent] = nil
-            Family[Ent] = true
+				Ent.CFW.Ancestor = Ancestor
+			end
 
-            Ent.CFW.Ancestor = Ancestor
-        end
+			Family[A] = true
 
-        A.CFW.Family = nil
-    end
+			Ancestor.CFW.Contraption.Families[AncFam] = nil
+			Ancestor.CFW.Family = Family
+		else
+			for Ent in pairs(Family) do
+				AncFam[Ent] = true
+				Family[Ent] = nil
 
-    Ancestor.CFW.Family = Family -- Asign the family to the ancestor
-    A.CFW.Ancestor      = Ancestor -- Update A's ancestor
+				Ent.CFW.Ancestor = Ancestor
+			end
 
-    A.CFW.Contraption.Families[Family] = true -- Add this family to the contraption
+			AncFam[A] = true
+
+			A.CFW.Contraption.Families[Family] = nil
+		end
+	elseif Family then -- Transfer
+		print("Transfer family")
+		for Ent in pairs(Family) do
+			Ent.CFW.Ancestor = Ancestor
+		end
+
+		Ancestor.CFW.Family = Family
+		Family[A]           = true
+
+	elseif AncFam then -- Append
+		AncFam[A] = true
+	else -- New
+		print("New family")
+		local F = {[A] = true}
+
+		Ancestor.CFW.Family = F
+		Ancestor.CFW.Contraption.Families = Ancestor.CFW.Contraption.Families or {}
+
+		Ancestor.CFW.Contraption.Families[F] = true
+	end
+
+	A.CFW.Ancestor = Ancestor
+	A.CFW.Family   = nil
 end)
 
-hook.Add("OnContraptionDisconnect", "Families", function(A, B, Parent)
-    if not Parent then return end -- Only care about parent actions
+hook.Add("OnContraptionDisconnect", "Families", function(A, _, Parent)
+	if not Parent then return end -- Only care about parent actions
 
-    local OldAncestor = A.CFW.Ancestor
-    local OldFamily   = OldAncestor.CFW.Family
+	local Ancestor = A:GetAncestor()
+	local Family   = Ancestor:GetFamily()
 
-    OldFamily[A]   = nil -- Remove A from the old family
-    A.CFW.Ancestor = nil -- A is the ancestor now
+	Family[A] = nil -- Remove A from the family
 
-    local Subtree = GetAllChildren(A)
+	if next(Family) then -- Still something in the family
+		local AncFam   = Ancestor:GetFamily()
+		local Children = {}; A:GetAllChildren(Children) -- Outputs to Children
 
-    if next(Subtree) then -- If A has children, move them to A's family
-        local NewFamily = {}
+		if next(Children) then -- Transfer A's family and remove from Ancestor's
+			print("Inherit")
+			Children[A] = nil -- A is not a child (GetAllChildren includes it)
 
-        for Ent in pairs(Subtree) do
-            OldFamily[Ent] = nil
-            NewFamily[Ent] = true
+			for Ent in pairs(Children) do -- Remove from Ancestor's family
+				AncFam[Ent]      = nil
+				Ent.CFW.Ancestor = A
+			end
 
-            Ent.CFW.Ancestor = A
-        end
+			A.CFW.Family = Children
+			A.CFW.Contraption.Families[Children] = true
 
-        A.CFW.Family = NewFamily
-
-        if not A.CFW.Contraption.Families then
-            print("CFW")
-            PrintTable(A.CFW)
-            print("Contraption")
-            PrintTable(A.CFW.Contraption)
-        end
-        A.CFW.Contraption.Families[NewFamily] = true
-    end
+			if not next(AncFam) then -- Ancestor's family is now empty
+				Ancestor.CFW.Contraption.Families[AncFam] = nil
+				Ancestor.CFW.Family = nil
+			end
+		end
+	else
+		Ancestor.CFW.Family = nil
+		Ancestor.CFW.Contraption.Families[Family] = nil
+	end
 end)
 
 hook.Add("OnContraptionSplit", "Families", function(Old, New) -- Transfer families that have been split off
-    if Old.Families then
-        New.Families = New.Families or {}
+	if Old.Families then -- If there are any families
+		New.Families = New.Families or {}
 
-        for Family in pairs(Old.Families) do
-            Old.Families[Family] = nil
-            New.Families[Family] = true
-        end
-    end
+		for Family in pairs(Old.Families) do -- Go over each one
+			if New.Ents[next(Family):GetAncestor()] then -- If the ancestor is in the new contraption
+				Old.Families[Family] = nil
+				New.Families[Family] = true -- Transfer family to the new one
+			end
+		end
+	end
 end)
 
 hook.Add("OnContraptionMerge", "Families", function(Kept, Removed) -- Transfer families from Removed to Kept
-    if Removed.Families then
-        for Family in pairs(Removed.Families) do
-            Removed.Families[Family] = nil
-            Kept.Families[Family]    = true
-        end
+	if Removed.Families then
+		Kept.Families = Kept.Families or {}
 
-        Removed.Families = nil
-    end
+		for Family in pairs(Removed.Families) do
+			Removed.Families[Family] = nil
+			Kept.Families[Family]    = true
+		end
+
+		Removed.Families = nil -- Removed is getting deleted anyways, but just in case
+	end
 end)
 
 hook.Add("Initialize", "CFW Families", function()
-    local ENT = FindMetaTable("Entity")
+	local ENT = FindMetaTable("Entity")
 
-    function ENT:GetAncestor()
-        if self.CFW then
-            return self.CFW.Ancestor or self
-        else
-            return self
-        end
-    end
+	function ENT:GetAncestor()
+		if self.CFW then
+			return self.CFW.Ancestor or self
+		else
+			return self
+		end
+	end
 
-    hook.Remove("Initialize", "CFW Families")
+	function ENT:GetFamily()
+		return self.CFW and self.CFW.Family or nil
+	end
+
+	function ENT:GetAllChildren(Output)
+		if Output then
+			Output[self] = true
+		else
+			Output = {}
+		end
+
+		for K in pairs(self:GetChildren()) do
+			K:GetAllChildren(Output)
+		end
+
+		return Output
+	end
+
+	hook.Remove("Initialize", "CFW Families")
 end)
