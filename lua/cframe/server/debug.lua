dcf93e94236162ca6b43f76d68899c988c050292
TwistedTail <8784231+TwistedTail@users.noreply.github.com>
2020-07-18 02:17:00 -0400

    Improved debug module
    Debug module now uses a convar to define whether it's active or not. Will be disabled by default.

--- lua/cframe/server/debug.lua
+++ lua/cframe/server/debug.lua
@@ -1,39 +1,34 @@
-hook.Add("OnContraptionCreated", "CFW Debug", "CFW Debug", function(C)
-    print("OnContraptionCreated", C)
+local Enabled = CreateConVar("cframe_debug", 0, FCVAR_ARCHIVE, "Enables console debugging of every CFrame hook", 0, 1)
+local Hooks = {
+    OnContraptionCreated = true,
+    OnContraptionDeleted = true,
+    OnContraptionConnect = true,
+    OnContraptionDisconnect = true,
+    OnContraptionAppend = true,
+    OnContraptionPop = true,
+    OnContraptionInit = true,
+    OnContraptionState = true,
+    OnContraptionSplit = true,
+    OnContraptionMerge = true
+}
+
+local function Callback(_, _, Value)
+    local Active = tobool(Value)
+    local Function = Active and hook.Add or hook.Remove
+
+    for K in pairs(Hooks) do
+        Function(K, "CFW Debug", Active and function(...)
+            print(K, ...)
+        end)
+    end
+end
+
+hook.Add("Initialize", "CFW Debug", function()
+    if Enabled:GetBool() then
+        Callback(nil, nil, true)
+    end
+
+    hook.Remove("Initialize", "CFW Debug")
 end)
 
-hook.Add("OnContraptionDeleted", "CFW Debug", function(C)
-    print("OnContraptionDeleted", C)
-end)
-
-hook.Add("OnContraptionConnect", "CFW Debug", function(C, A, B, Parent)
-    print("OnContraptionConnect", C, A, B, Parent)
-end)
-
-hook.Add("OnContraptionDisconnect", "CFW Debug", function(C, A, B, Parent)
-    print("OnContraptionDisconnect", C, A, B, Parent)
-end)
-
-hook.Add("OnContraptionAppend", "CFW Debug", function(C, A)
-    print("OnContraptionAppend", C, A)
-end)
-
-hook.Add("OnContraptionPop", "CFW Debug", function(C, A)
-    print("OnContraptionPop", C, A)
-end)
-
-hook.Add("OnContraptionInit", "CFW Debug", function(A)
-    print("OnContraptionInit", A)
-end)
-
-hook.Add("OnContraptionState", "CFW Debug", function(A, State)
-    print("OnContraptionState", A, State)
-end)
-
-hook.Add("OnContraptionSplit", "CFW Debug", function(Old, New)
-    print("OnContraptionSplit", Old, New)
-end)
-
-hook.Add("OnContraptionMerge", "CFW Debug", function(Kept, Removed)
-    print("OnContraptionMerge", Kept, Removed)
-end)
+cvars.AddChangeCallback("cframe_debug", Callback)
